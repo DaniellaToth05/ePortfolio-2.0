7b5df2bf2fd625e26ad62c201a58e9d3ab7b5124
Daniella Toth <daniellatoth@me.com>
2025-12-22 16:31:42 -0500

    set up base files for true frontend

--- frontend/eportfolio-ui/src/main.js
+++ frontend/eportfolio-ui/src/main.js
@@ -1,9 +1,6 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
-import './styles/base.css'
-import './styles/components.css'
-import './styles/components.css'
 
 
 const app = mount(App, {
