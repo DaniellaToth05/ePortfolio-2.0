b5615603c4d5e076425239f14cbbcd3b44aa4fb3
Daniella Toth <daniellatoth@me.com>
2025-12-21 23:50:16 -0500

    init spring boot backend with health endpoint

---
+++ backend/eportfolio/src/test/java/com/daniella/eportfolio/EportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniella.eportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
