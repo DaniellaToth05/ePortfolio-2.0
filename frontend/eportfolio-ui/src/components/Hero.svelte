4ef251db79d36227116e6dc24269d82e33f7134c
Daniella Toth <daniellatoth@me.com>
2025-12-22 18:32:57 -0500

    add hero section component

---
+++ frontend/eportfolio-ui/src/components/Hero.svelte
@@ -0,0 +1,18 @@
+<section class="hero">
+    <div class="container">
+      <div class="hero-content">
+        <!-- LEFT -->
+        <div class="hero-text">
+          <div class="greeting">Good Afternoon, Daniella</div>
+          <h1>Your Investments, Simplified.</h1>
+          <p class="subtitle">
+            Stay on top of your portfolio — down to the last dollar.
+          </p>
+        </div>
+  
+        <!-- RIGHT (EMPTY FOR NOW) -->
+        <div></div>
+      </div>
+    </div>
+  </section>
+  
