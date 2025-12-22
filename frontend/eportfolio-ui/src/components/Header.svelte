3151b81bb50bf04aed2b1b32bbf825954abccbe3
Daniella Toth <daniellatoth@me.com>
2025-12-22 16:36:52 -0500

    add header component

---
+++ frontend/eportfolio-ui/src/components/Header.svelte
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="container">
+      <nav class="nav">
+        <div class="logo">ePortfolio</div>
+  
+        <div class="nav-links">
+          <a href="#" class="nav-link">Dashboard</a>
+          <a href="#" class="nav-link">Portfolio</a>
+          <a href="#" class="nav-link">Analytics</a>
+          <a href="#" class="nav-link">Goals</a>
+          <a href="#" class="nav-link">Insights</a>
+        </div>
+      </nav>
+    </div>
+  </header>
+  
